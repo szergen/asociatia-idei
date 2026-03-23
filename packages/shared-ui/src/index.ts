892553514176f81a2494628aef28488c8959a3e6
szergen <szergen@gmail.com>
2026-03-23 16:30:13 +0200

    fix: Fixed types

--- packages/shared-ui/src/index.ts
+++ packages/shared-ui/src/index.ts
@@ -3,14 +3,7 @@
 // Export components
 export { default as Button } from "./components/Button";
 export { default as Header } from "./components/Header";
-export { default as Footer } from "./components/Footer";
-export { default as Card } from "./components/Card";
-
-// Export hooks
-export { default as useLocalStorage } from "./hooks/useLocalStorage";
 
 // Export types
 export type { ButtonProps } from "./components/Button";
 export type { HeaderProps } from "./components/Header";
-export type { FooterProps } from "./components/Footer";
-export type { CardProps } from "./components/Card";
