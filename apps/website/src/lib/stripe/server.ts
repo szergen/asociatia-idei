80aa487765ce219f8fa227519bdbdb10b080d418
szergen <szergen@gmail.com>
2026-03-23 16:25:41 +0200

    fix: Fixed stripe ts

--- apps/website/src/lib/stripe/server.ts
+++ apps/website/src/lib/stripe/server.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  // @ts-ignore - Bypassing strict version check caused by Docker npm install drift
   apiVersion: "2024-12-18.acacia",
   typescript: true,
 });
